ab18e0d3e6c7ffbb1650a513e01e08f389fc75bc
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-08 19:09:09 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
