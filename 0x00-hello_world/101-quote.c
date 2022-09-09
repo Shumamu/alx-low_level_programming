238c2109d6673926f8cef7aa70e27652fbc2597f
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-08 19:59:02 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Print out the last part of a quote in the standard error
+ *
+ * Description: print Dora's quote
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
