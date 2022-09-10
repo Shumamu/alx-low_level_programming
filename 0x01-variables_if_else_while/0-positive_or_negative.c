78bac38d6ac899552fa72e5ae5fd5e4c1749874f
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-10 06:58:43 -0700

    porn

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,25 +5,24 @@
 /**
  * main - determine if the number is positive or negative
  *
- * Description; Assign a random number to the variable
+ * Description: Assign a random number to the variable
  * n each time it is executed
  *
  * Return - Always 0 (Success)
  */
 
 int main(void)
-
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
-}	
+}
