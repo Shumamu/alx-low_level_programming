6ade94748bd28e1de64851e930866890573a039b
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-11 15:36:59 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - print triple combos
+ *
+ * Descriptin: print triple combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			if (i < 55 || j < 56 || k < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			k++
+		}
+		j++
+	}
+	i++
+}
+putchar(10);
+
+return (0);
+}
