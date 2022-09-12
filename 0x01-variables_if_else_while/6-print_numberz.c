81e668d1dc2de10a85ae675a4f0875cf18a3d11c
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-11 17:07:57 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: I really dont care
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
