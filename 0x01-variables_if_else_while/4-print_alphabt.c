4b93ada2cdcbd47f37c2cd8512e9936df2f984f6
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-11 17:23:42 -0700

    alpahbet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets
+ *
+ * Description: print
+ * the
+ * alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
