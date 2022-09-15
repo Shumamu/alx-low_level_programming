742f6abc8f52e160b277e8a76927438d7fd66652
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-15 07:04:41 -0700

    !main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("infinte loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * }
-	 *    putchar(i);
-	 * }
-	 */
+
+	/* while (i <10) */
+	/* { */
+	/*   putchar(i); */
+	/* } */
+
 	printf("infinte loop avoided! \\o/\n");
 
 	return (0);
