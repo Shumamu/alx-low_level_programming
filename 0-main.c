10eb6d5de61c07b2c992faffbf6f8d24aa2684b1
Osijo Adedeji <osijoadedeji@gmail.com>
2022-09-15 06:00:21 -0700

    main.c

---
+++ 0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - testd function that prints if integer is positve or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
