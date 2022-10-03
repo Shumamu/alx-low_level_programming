1bcab190b50d19d5fa7bce06e3daa734e9d12e3b
Osijo Adedeji <osijoadedeji@gmail.com>
2022-10-03 16:25:21 -0700

    static lib

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src into dest
+ * @dest: destination
+ * @src: source
+ * Return: char with copy of string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a, b = 0;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[b] = src[a];
+		b++;
+	}
+	dest[b] = '\0';
+
+	return (dest);
+}
