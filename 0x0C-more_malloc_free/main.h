8a053f3e778c1a509986bd044a28240d6def1586
Osijo Adedeji <osijoadedeji@gmail.com>
2022-10-06 15:36:55 -0700

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+int check_number(char *str);
+
+unsigned int string_length(char *str);
+
+void print_string(char *str);
+
+void *_calloc(unsigned int number, unsigned int size);
+
+int _putchar(char c);
+
+
+
+
+
+#endif
