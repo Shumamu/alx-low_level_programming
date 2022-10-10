e5ecc33528f9fa2ee40bc8b9be8a25ad6d609b01
Osijo Adedeji <osijoadedeji@gmail.com>
2022-10-10 16:32:18 -0700

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * free_dog - free the dogs
+ * @d: dog
+ */
+
+
+
+void free_dog(dog_t *d)
+
+{
+
+	if (d != NULL)
+
+	{
+
+		free(d->name);
+
+		free(d->owner);
+
+		free(d);
+
+	}
+
+}
