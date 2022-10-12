b567ccbb0be339f04d878daae9a657c7967b74bf
Osijo Adedeji <osijoadedeji@gmail.com>
2022-10-12 14:50:16 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "fucntion_pointers.h"
+
+/**
+ * print_name - function to print name
+ *
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+void print_name(char *name, void (*f)(char *))
+
+{
+
+	if (name && f)
+
+		f(name);
+
+}
