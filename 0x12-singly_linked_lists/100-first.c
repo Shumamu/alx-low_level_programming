c5bb0ae8c5cf9c447c3db1654f4fbfc82e4238f4
Osijo Adedeji <osijoadedeji@gmail.com>
2022-10-20 20:52:06 -0700

    linkedly list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * first - function executed before main
+ * Return: no return.
+ */
+
+
+
+void __attribute__ ((constructor)) first()
+
+{
+
+	printf("You're beat! and yet, you must allow");
+
+	printf(",\nI bore my house upon my back!\n");
+
+}
